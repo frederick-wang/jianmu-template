a4f9d8861e8630e96664d9a639ed93e66a27cf4c
Zhaoji Wang <hwoam@outlook.com>
2023-01-18 08:03:32 +0800

    test

--- src/app.py
+++ src/app.py
@@ -1,2 +1,21 @@
-def say_hello(name):
-    return f'Hello, {name}!'
+from jianmu import ref, computed
+from ltp import LTP
+
+ltp = LTP()
+
+count = ref(0)
+# 创建一个响应式变量 name，初始值为 'World'
+name = ref('World')
+# 创建一个响应式变量 result
+result = computed(lambda: {
+    'message': f'Hello {name.value}',
+    'count': count.value,
+})
+
+
+def add_count():
+    count.value += 1
+
+sentence = ref('他叫汤姆去拿外衣')
+
+sent_split = computed(lambda: ltp.pipeline([str(sentence.value)], tasks = ["cws"], return_dict = False)[0])
---
+++ ui/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { pyfuncs, pyvars } from 'jianmu'
+
+const { name, result, sentence, sent_split } = pyvars
+const { add_count } = pyfuncs
+</script>
+
+<template>
+  <div id="app">
+    name: <input v-model="name" placeholder="请输入 name 的值" clearable />
+    <p>
+      <button @click="add_count()">调用 add_count()</button>
+    </p>
+    <p>result 的值为：</p>
+    <pre>{{ result }}</pre>
+    原句：<input v-model="sentence" placeholder="请输入 name 的值" clearable />
+    <p>分词结果为：</p>
+    <pre>{{ sent_split }}</pre>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+#app {
+  padding: 20px;
+}
+</style>
